fe67b3ee66a90c72722fdce17b3a0324890a418d
Gianna Cosenza <giacosey28@gmail.com>
2026-07-01 14:37:07 -0400

    added variable with array

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,9 @@ function App() {
           <li key={index}>{fruit}</li>
         ))}
       </ul>
+      <p>Fruit 1: {fruits[0]}</p>
+      <p>Fruit 2: {fruits[1]}</p>
+      <p>Fruit 3: {fruits[2]}</p>
       <p>name: {person1.name}</p>
       <p>age: {person1.age}</p>
     </div>
