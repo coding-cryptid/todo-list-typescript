bbbb79726fef7a0f773fe8f199756ab7d9117fcb
Gianna Cosenza <giacosey28@gmail.com>
2026-07-01 14:41:55 -0400

    more practicing in the sandbox

--- src/App.tsx
+++ src/App.tsx
@@ -16,22 +16,45 @@ function App() {
     age: 26,
   };
 
+  const shapeShifter: any = {
+    name: 24,
+    age: 'Alex'
+  }
+
+  const printToConsole = (value: string): void => {
+    console.log(value);
+  }
+
+  const cat: null = null;
+  const dog: undefined = undefined;
+  console.log('cat ',cat);
+  console.log('dog',dog);
+
   return (
     <div>
       <p>Full Name: {fullName}</p>
       <p>Age: {year}</p>
       <p>Is this BootCamp Fun?: {isThisBootCampFun ? 'Yes' : 'No'}</p>
+
       <p>Fruits:</p>
       <ul>
         {fruits.map((fruit, index) => (
           <li key={index}>{fruit}</li>
         ))}
       </ul>
+
       <p>Fruit 1: {fruits[0]}</p>
       <p>Fruit 2: {fruits[1]}</p>
       <p>Fruit 3: {fruits[2]}</p>
+
       <p>name: {person1.name}</p>
       <p>age: {person1.age}</p>
+
+      <button onClick={() => printToConsole('Hello, TypeScript!')}>Click me</button>
+      <p>name: {shapeShifter.name}</p>
+      <p>age: {shapeShifter.age}</p>
+      <p>cat: {cat}</p>
+      <p>dog: {dog}</p>
     </div>
   )
 }
